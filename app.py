5a56bb4bb3869cb6455934f6f3e5ae55bdb5459c
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-09-29 03:14:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from gradio_client import Client
 import streamlit as st
 import os
-from dotenv import 
-
-
-user_query=st.text_input("Enter your query here: ")
+from dotenv import load_dotenv
+load_dotenv()
 hf_token=os.getenv('hf_token')
+user_query=st.text_input("Enter your query here: ")
 client = Client("https://binqiangliu-llama2-txt-gen.hf.space/")
 result = client.predict(
 				user_query,	# str in 'prompt' Textbox component
