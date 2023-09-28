5a348ecc3d31b0caf9858140084d3a79a82aefef
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-09-29 03:34:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
 hf_token=os.getenv('hf_token')
+
 user_query=st.text_input("Enter your query here: ")
 client = Client("https://binqiangliu-llama2-txt-gen.hf.space/")
-result = client.predict(user_query, api_name="/predict")
-st.write("AI Reponse: "+result)
+if user_query !="" and not user_query.strip().isspace() and not user_query == "" and not user_query.strip() == "" and not user_query.isspace():
+  result = client.predict(user_query, api_name="/predict")
+  st.write("AI Reponse: "+result)
+else:
+  st.write("Enter your query first.")
+  st.stop()
