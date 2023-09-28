910a8a96fe29dd5fe26d158b5bdfbc22510b2f55
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-09-29 03:42:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from gradio_client import Client
 import streamlit as st
 import os
+import sys
 from dotenv import load_dotenv
+import path
+from streamlit.components.v1 import html
 load_dotenv()
+
+st.set_page_config(page_title="Cheers! Open AI Doc-Chat Assistant", layout="wide")
+st.subheader("Open AI Doc-Chat Assistant: Life Enhancing with AI!")
+
+css_file = "main.css"
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
 hf_token=os.getenv('hf_token')
 
